62eeedb518f5a0502d867608dd1c536cccfa863f
J0see1 <marcelalvin11@gmail.com>
2023-11-30 14:07:34 +0700

    test

--- frontend-nuxt/tailwind.config.js
+++ frontend-nuxt/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     fontFamily: {
       'sans': ['Helvetica', 'Arial', 'sans-serif'],
-      'logo': ['Poppins', 'sans-serif']
+      'logo': ['Kavoon', 'sans-serif']
     },
   },
   plugins: [],
