39384f262b68458bdd093095e868be9057c437cf
J0see1 <marcelalvin11@gmail.com>
2023-12-02 22:49:54 +0700

    fix: about page

--- frontend-nuxt/pages/about.vue
+++ frontend-nuxt/pages/about.vue
@@ -7,31 +7,31 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <!-- Team Member Card 1 -->
                 <div class="bg-white rounded-lg shadow-md overflow-hidden">
-                    <img src="frontend-nuxt\assets\images\marcel.jpeg" alt="Team Member" class="w-full h-56 object-cover object-center">
+                    <img src="../assets/images/marcel.jpeg" alt="Team Member" class="w-full h-1/2 object-cover object-center">
                     <div class="p-4">
-                        <h2 class="text-xl font-semibold mb-2">Marcelinus Alvinanda Chrisantya</h2>
-                        <p class="text-gray-700 mb-4">5027221012</p>
-                        <p class="text-gray-600">"Fish on, mancing mania mantap"</p>
+                        <h2 class="text-xl text-black font-bold mb-2">Marcelinus Alvinanda Chrisantya</h2>
+                        <p class="text-black mb-4">5027221012</p>
+                        <p class="text-black">"Fish on, mancing mania mantap"</p>
                     </div>
                 </div>
 
                 <!-- Team Member Card 2 -->
                 <div class="bg-white rounded-lg shadow-md overflow-hidden">
-                    <img src="frontend-nuxt\assets\images\gebi.jpeg" alt="Team Member" class="w-full h-56 object-cover object-center">
+                    <img src="../assets/images/gebi.jpeg" alt="Team Member" class="w-full h-1/2 object-cover object-center">
                     <div class="p-4">
-                        <h2 class="text-xl font-semibold mb-2">Gabriella Erlinda Wijaya</h2>
-                        <p class="text-gray-700 mb-4">5027221018</p>
-                        <p class="text-gray-600">"I once believed love would be burning red, but it's golden"</p>
+                        <h2 class="text-xl text-black font-bold mb-2">Gabriella Erlinda Wijaya</h2>
+                        <p class="text-black mb-4">5027221018</p>
+                        <p class="text-black">"I once believed love would be burning red, but it's golden"</p>
                     </div>
                 </div>
 
                 <!-- Team Member Card 3 -->
                 <div class="bg-white rounded-lg shadow-md overflow-hidden">
-                    <img src="frontend-nuxt\assets\images\jojo.jpeg" alt="Team Member" class="w-full h-56 object-cover object-center">
+                    <img src="../assets/images/jojo.jpeg" alt="Team Member" class="w-full h-1/2 object-cover object-center">
                     <div class="p-4">
-                        <h2 class="text-xl font-semibold mb-2">Jonathan Adithya Baswara</h2>
-                        <p class="text-gray-700 mb-4">5027221062</p>
-                        <p class="text-gray-600">"Hooh"</p>
+                        <h2 class="text-xl text-black font-bold mb-2">Jonathan Adithya Baswara</h2>
+                        <p class="text-black mb-4">5027221062</p>
+                        <p class="text-black">"Hooh"</p>
                     </div>
                 </div>
             </div>
